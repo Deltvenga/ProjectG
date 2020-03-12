91d5ab8a7b878bc6caf78e720c2175dd929574ae
thelyaa <thelyaa@yandex.ru>
2020-03-12 15:50:00 +0500

    исправлен метод получения типов забегов, метод получения всех предыдущих результатов

--- WcfServiceLibrary/WcfServiceLibrary1/Service1.cs
+++ WcfServiceLibrary/WcfServiceLibrary1/Service1.cs
@@ -50,14 +50,14 @@ namespace WcfServiceLibrary1
         /// Метод получения всех типов заездов в каждом из марафоне
         /// </summary>
         /// <returns>Список всех типов заездов в каждом марафоне</returns>
-        public List<List<string>> GetEventType()
+        public List<List<string>> GetEventType(int idMarathon)
         {
             //string connectionString = "Data Source=LAPTOP-20V122MK;Integrated Security=SSPI;Initial Catalog=marathondb";
             SqlConnection con = new SqlConnection(getConString());
             con.Open();
             SqlCommand cmd = con.CreateCommand();
             cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "Select EventType.EventTypeName, Marathon.MarathonId From EventType, [Event], Marathon Where [Event].MarathonId = Marathon.MarathonId and EventType.EventTypeId = [Event].EventTypeId;";
+            cmd.CommandText = "Select EventType.EventTypeName, Marathon.MarathonId From EventType, [Event], Marathon Where [Event].MarathonId = Marathon.MarathonId and Marathon.MarathonId = '" + idMarathon + "' and EventType.EventTypeId = [Event].EventTypeId;";
 
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
@@ -106,7 +106,16 @@ namespace WcfServiceLibrary1
             return list;
         }
 
-        public List<List<string>> GetAge(int fromAge, int toAge, int idMarathon, string idEventType, string gender)
+        /// <summary>
+        /// Метод получения всех бегунов по определенным параметрам
+        /// </summary>
+        /// <param name="fromAge">нижний возрастной порог</param>
+        /// <param name="toAge">верхний возрастной порог</param>
+        /// <param name="idMarathon">номер марафона</param>
+        /// <param name="idEventType">тип забега</param>
+        /// <param name="gender">выбранный пол</param>
+        /// <returns></returns>
+        public List<List<string>> GetPreviousResult(int fromAge, int toAge, int idMarathon, string idEventType, string gender)
         {
             var fromDate = getDateFormat(fromAge);
             var toDate = getDateFormat(toAge);
@@ -116,8 +125,8 @@ namespace WcfServiceLibrary1
             SqlCommand cmd = con.CreateCommand();
             cmd.CommandType = CommandType.Text;
             //cmd.CommandText = "set language english Select RegistrationEvent.RaceTime, [User].FirstName, [User].LastName, Runner.DateOfBirth, Runner.CountryCode From [User], Runner, RegistrationEvent, Registration Where Runner.RunnerId = Registration.RunnerId and Registration.RegistrationId = RegistrationEvent.RegistrationId and [User].Email = Runner.Email and RaceTime is not null and RaceTime <> 0 and Runner.DateOfBirth between '" + toDate + "' and '" + fromDate + "' order by RaceTime;";
-            if (gender != "Any") cmd.CommandText = "set language english Select RegistrationEvent.RaceTime, [User].FirstName, [User].LastName, Runner.DateOfBirth, Runner.CountryCode From [User], [Event], Runner, RegistrationEvent, Registration Where Runner.RunnerId = Registration.RunnerId and Registration.RegistrationId = RegistrationEvent.RegistrationId and [User].Email = Runner.Email and RaceTime is not null and RaceTime <> 0 and[Event].EventTypeId = '" + idEventType + "' and[Event].MarathonId = '" + idMarathon + "' and Runner.Gender = '" + gender + "' and Runner.DateOfBirth between '" + toDate + "' and '" + fromDate + "' order by RaceTime;";
-            else cmd.CommandText = "set language english Select RegistrationEvent.RaceTime, [User].FirstName, [User].LastName, Runner.DateOfBirth, Runner.CountryCode From [User], [Event], Runner, RegistrationEvent, Registration Where Runner.RunnerId = Registration.RunnerId and Registration.RegistrationId = RegistrationEvent.RegistrationId and [User].Email = Runner.Email and RaceTime is not null and RaceTime <> 0 and[Event].EventTypeId = '" + idEventType + "' and[Event].MarathonId = '" + idMarathon + "' and Runner.DateOfBirth between '" + toDate + "' and '" + fromDate + "' order by RaceTime;";
+            if (gender != "Any") cmd.CommandText = "set language english Select RegistrationEvent.RaceTime, concat([User].FirstName, ' ', [User].LastName) as RunnerName, Runner.DateOfBirth, Runner.CountryCode From [User], [Event], Runner, RegistrationEvent, Registration Where Runner.RunnerId = Registration.RunnerId and Registration.RegistrationId = RegistrationEvent.RegistrationId and [User].Email = Runner.Email and RaceTime is not null and RaceTime <> 0 and[Event].EventTypeId = '" + idEventType + "' and[Event].MarathonId = '" + idMarathon + "' and Runner.Gender = '" + gender + "' and Runner.DateOfBirth between '" + toDate + "' and '" + fromDate + "' order by RaceTime;";
+            else cmd.CommandText = "set language english Select RegistrationEvent.RaceTime, concat([User].FirstName, ' ', [User].LastName) as RunnerName, Runner.DateOfBirth, Runner.CountryCode From [User], [Event], Runner, RegistrationEvent, Registration Where Runner.RunnerId = Registration.RunnerId and Registration.RegistrationId = RegistrationEvent.RegistrationId and [User].Email = Runner.Email and RaceTime is not null and RaceTime <> 0 and[Event].EventTypeId = '" + idEventType + "' and[Event].MarathonId = '" + idMarathon + "' and Runner.DateOfBirth between '" + toDate + "' and '" + fromDate + "' order by RaceTime;";
 
             SqlDataAdapter da = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
@@ -131,8 +140,7 @@ namespace WcfServiceLibrary1
                 List<string> rowlist = new List<string>();
                 rowlist.Add(index.ToString());
                 rowlist.Add(dtreader["RaceTime"].ToString());
-                rowlist.Add(dtreader["FirstName"].ToString());
-                rowlist.Add(dtreader["LastName"].ToString());
+                rowlist.Add(dtreader["RunnerName"].ToString());
                 rowlist.Add(dtreader["DateOfBirth"].ToString());
                 rowlist.Add(dtreader["CountryCode"].ToString());
                 list.Add(rowlist);
@@ -143,7 +151,11 @@ namespace WcfServiceLibrary1
             return list;
         }
 
-       
+        /// <summary>
+        /// Метод приведения дня рождения в формат SQL даты 
+        /// </summary>
+        /// <param name="age">Возраст бегунов</param>
+        /// <returns>Строковое представление даты</returns>
         private string getDateFormat(int age)
         {
             var date = getDate(age);
@@ -153,6 +165,11 @@ namespace WcfServiceLibrary1
             return (year + '-' + month + '-' + day).ToString();
         }
 
+        /// <summary>
+        /// Метод получения дня рождения бегунов для выборки забегов
+        /// </summary>
+        /// <param name="age">возраст бегунов</param>
+        /// <returns>день рождения</returns>
         private DateTime getDate(int age)
         {
             var timeNow = DateTime.Now;
